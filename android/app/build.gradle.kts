f93ebd609dab9e38fd4290449899bfc76cc8b099
OsamaHamad123 <osamahamad665@gmail.com>
2025-10-12 01:05:08 +0300

    fix some bugs about gradle and flavors

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.android")   // صح
     id("dev.flutter.flutter-gradle-plugin")
+    // لو Firebase: id("com.google.gms.google-services")
 }
-
 android {
     namespace = "com.example.doc_doc_app"
-
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -14,48 +13,43 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
     defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.doc_doc_app"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
-
-    
-    buildTypes {
-      getByName("debug") {
-        applicationIdSuffix = ".debug"
-        versionNameSuffix = "-debug"
-        
-      }
-      getByName("release") {
-        signingConfig = signingConfigs.getByName("debug")
-        proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        applicationIdSuffix = ".release"
-        versionNameSuffix = "-release"
-      }
-    }
-    flavorDimensions += "default"
+     flavorDimensions += "default"
     productFlavors {
         create("development") {
             dimension = "default"
-            applicationIdSuffix = ".staging"  
-            resValue("string", "app_name", "Flavors development")
+            applicationIdSuffix = ".dev"
+            resValue("string", "app_name", "DocDoc Dev")
         }
         create("production") {
             dimension = "default"
-            applicationIdSuffix = ".production"
-            resValue("string", "app_name", "Flavors Production")
+            resValue("string", "app_name", "DocDoc Prodaction")
+            
+        }
+    }
+
+    buildTypes {
+        release {
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
- 
-
 
 flutter {
     source = "../.."
